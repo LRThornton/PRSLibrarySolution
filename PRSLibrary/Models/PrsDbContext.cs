0591e709049ff2c9486c9d4bab227ce05fec56fc
leslie thornton <shodge51@aol.com>
2022-02-14 10:42:56 -0500

    Add project files.

---
+++ PRSLibrary/Models/PrsDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRSLibrary.Models {
+    public class PrsDbContext : DbContext {
+
+        public PrsDbContext() { }
+
+        public PrsDbContext(DbContextOptions<PrsDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) {
+            if(!builder.IsConfigured) {// if not already configured do the following
+                builder.UseSqlServer(
+                    "server=localhost\\sqlexpress;database =TestPrsDb;trusted_connection=true"
+                    );
+
+            }
+        }
+    }
+}
