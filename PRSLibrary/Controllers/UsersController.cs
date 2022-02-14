cf8deee91c6523156ef12a2aaaa6470d1e312fe7
leslie thornton <shodge51@aol.com>
2022-02-14 16:12:30 -0500

    Added Users Controller

---
+++ PRSLibrary/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using PRSLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRSLibrary.Controllers {
+    public class UsersController {
+
+        private readonly PrsDbContext _context;  //this is an internal variable for this class only
+
+        public UsersController(PrsDbContext context) {
+            this._context = context;
+            
+        }
+        //this method returns all of the users
+        public IEnumerable<User> GetAll(UsersController userCtrl) {
+            return _context.Users.ToList();
+        }
+        //this method lets you pull by primary key
+        public User GetByPk(int id) {
+            return _context.Users.Find(id);
+        }
+
+        public User Create(User user) {
+            if(user is null) {
+                throw new ArgumentNullException("user");
+            }
+            if(user.Id !=0) {
+                throw new ArgumentException("User.Id must be zero!");
+            }
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user;
+        }
+    }
+}
--- TestPRSLibrary/Program.cs
+++ TestPRSLibrary/Program.cs
@@ -1,9 +1,44 @@
-﻿using System;
+﻿using PRSLibrary.Controllers;
+using PRSLibrary.Models;
+using System;
 
 namespace TestPRSLibrary {
     class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+        public static void Main(string[] args) {
+
+
+            var context = new PrsDbContext();
+
+            var userCtrl = new UsersController(context);
+
+            var newUser = new User() {
+                Id = 0, Username = "xx", Password = "xx",
+                Firstname = "User", Lastname = "XX", Phone = "211", Email = "xx@user.com", 
+                IsReviewer = false, IsAdmin = false
+            };
+
+            userCtrl.Create(newUser);
+        
+            var user3 = userCtrl.GetByPk(3);
+            if(user3 is null) {
+                Console.WriteLine("User not found!");
+            } else {
+                Console.WriteLine($"User3: {user3.Firstname} {user3.Lastname}");
+            }
+             var user33 = userCtrl.GetByPk(33);
+            if (user33 is null) {
+                Console.WriteLine("User not found!");
+            } else {
+                Console.WriteLine($"User33: {user33.Firstname} {user33.Lastname}");
+            }
+
+            var users = userCtrl.GetAll(userCtrl);
+
+            foreach(var user in users) {
+                Console.WriteLine($"{user.Firstname} {user.Lastname}");
+
+            }
         }
     }
 }
